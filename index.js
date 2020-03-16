3155868a3b6902042780b4e017f9b4a9ae270956
Nikki Lin <nikkiylin@gmail.com>
2020-03-16 00:46:58 -0700

    Added a function in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('From index.js')
+const BUY_CAKE = 'BUY_CAKE'//declare the type of the action
+
+//define the action
+
+//an action is an Object with a type property
+//an action creater is function that returns an action  
+function buyCake() {
+    return {
+        type: BUY_CAKE, //type property
+        info: 'First redux action'
+    
+    }
+
+}
